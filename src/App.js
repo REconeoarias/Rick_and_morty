d75e6f5981a2f248a3f058a213a37c337d5f9cc2
RonaldConeo <ronadconeo103@gmail.com>
2023-07-13 11:07:27 -0500

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
+// import './App.css';
+import Card from './Card/Card.jsx';
+import Cards from './Cards/Cards.jsx';
+import SearchBar from './SearchBar/SearchBar.jsx';
 import characters, { Rick } from './data.js';
 
 function App() {
