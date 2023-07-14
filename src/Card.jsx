53901dde77e9676c6aeee2a7e0c63ff11b366e6c
RonaldConeo <ronadconeo103@gmail.com>
2023-07-13 21:43:05 -0500

    commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,29 @@
+import styles from './color.css'
+
 export default function Card(props) {
    return (
-      <div>
-         <button onClick={props.onClose}>X</button>
-         <h2>{props.name}</h2>
-         <h2>{props.status}</h2>
-         <h2>{props.species}</h2>
-         <h2>{props.gender}</h2>
-         <h2>{props.origin}</h2>
-         <img src={props.image} alt='rick sanchez' />
-      </div>
+      <div className={styles.container}>
+         // <button onClick={props.onClose}>X</button>
+         // <h2>{props.name}</h2>
+         // <h2>{props.status}</h2>
+         // <h2>{props.species}</h2>
+         // <h2>{props.gender}</h2>
+         // <h2>{props.origin}</h2>
+         // <img src={props.image} alt='rick sanchez' />
+      // </div>
    );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,8 +1,10 @@
+import styles from './searchBar.css'
+
 export default function SearchBar(props) {
    return (
       <div>
          <input type='search' />
-         <button onClick={props.onSearch}>Agregar</button>
+         <button className={styles.boton}  onClick={props.onSearch}>Agregar</button>
       </div>
    );
 }
