368302190567051880494900d443f02194879518
RonaldConeo <ronadconeo103@gmail.com>
2023-07-18 15:47:57 -0500

    commit

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,15 +1,16 @@
 import style from './color.module.css'
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({ id, name, status, species, gender, origin, image, onClose }) {
    return (
       <div className={style.container} >
-         <button className={style.btn} onClick={props.onClose}>X</button>
-         <img className={style.imagen} src={props.image}
+         <button className={style.btn} onClick={() => onClose(id)}>X</button>
+         <img className={image} src={name}
             alt='rick sanchez' />
-         <h2>{props.name}</h2>
-         <h2>{props.species}</h2>
-         <h2>{props.gender}</h2>
+         <h2>{name}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin}</h2>
       </div>
    );
 }
